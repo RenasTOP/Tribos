ca5b64daab6e03a5b27f1e260a01eca24c67f1d8
RenasTOP <irenasthat@gmail.com>
2025-05-08 04:21:24 +0200

    Update DefesaDisponivel.js

--- scripts/DefesaDisponivel.js
+++ scripts/DefesaDisponivel.js
@@ -58,8 +58,8 @@ $('<style>').prop('type','text/css').html(`
   }
   /* ícone dentro do botão: até 20×20 mas mantendo proporção */
   #sendToDiscord.btn-twf img {
-    max-width: 30px;
-    max-height: 30px;
+    max-width: 36px;
+    max-height: 36px;
     width: auto;
     height: auto;
     vertical-align: middle;
